579d4c418ba56f8617a20f639aed1bfd31b0b3c6
Guilherme Ramos <ramosguilhermefilipe@gmail.com>
2018-03-20 17:31:48 +0000

    Customized admin question list and form

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date Information',    {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently', 'number_of_choices')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
